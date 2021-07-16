8511d560d4577e0033509c5e87232c540f91e1c3
Poodle Master <69660581+PoodleMaster@users.noreply.github.com>
2021-07-16 13:45:39 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,74 @@
+############################################################
+# PoodleMaster
+############################################################
+from flask import Flask, request, render_template, jsonify
+from rembg.bg import remove
+import base64
+import json
+
+
+############################################################
+# Flask
+############################################################
+app = Flask(__name__)
+
+
+############################################################
+# index.html
+############################################################
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+############################################################
+# Exec.jsからPOSTされたときに動作
+############################################################
+@app.route('/output', methods=['POST'])
+def output():
+    # json形式でデータを受け取る
+    b64_pngdata = request.json['b64_pic']
+#   display(b64_pngdata, "b64_pngdata")
+
+    # base64デコード
+    tmpdata = b64_pngdata.split(',')        # base64のヘッダを削除
+    bindata = base64.b64decode(tmpdata[1])  # 「data:image/png;base64,～」以降のデータのみをデコード
+
+    # rembgでバックグランドを削除
+    contents = remove(bindata,
+                      alpha_matting=True,
+                      alpha_matting_foreground_threshold=240,
+                      alpha_matting_background_threshold=10,
+                      alpha_matting_erode_structure_size=10,
+                      alpha_matting_base_size=1000 )
+
+    # base64エンコード
+    tmpdata = str(base64.b64encode(contents))
+#   display(tmpdata, "tmpdata")
+    tmpdata = tmpdata[2:-1]  # 「ｂ’～’」の中身だけをエンコード
+
+    data1 = "data:image/png;base64," + tmpdata
+    data2 = "OK"
+
+    return_data = { "result_pic": data1,
+                    "result_okng": data2,
+                  }
+#   display(return_data, "return_data")
+    return jsonify(ResultSet=json.dumps(return_data))
+
+
+############################################################
+# debug用
+############################################################
+def display(data, name):
+    print(name)
+    print(data)
+    print(type(data))
+    print("")
+
+
+############################################################
+# flask起動
+############################################################
+if __name__ == '__main__':
+    app.run(port=9000, debug=False)
---
+++ static/Exec.js
@@ -0,0 +1,79 @@
+  //---------------------------------------------------------------------------
+  // Execボタン押下イベント
+  //---------------------------------------------------------------------------
+    function exec_button() {
+
+    document.getElementById('exec').disabled = true;
+    $('#ResultImg').css( { 'width' : '200' });
+    $('#exec').css( { 'cursor' : 'wait' });
+//  wait_pic = 'static/wait.gif?' + (new Date()).getTime()
+    wait_pic = 'static/wait.gif'
+    document.getElementById('ResultImg').src = wait_pic
+    const pic = document.getElementById('previewtImg').src;
+//  console.log(pic);
+
+    // JQueryによるPOST処理
+    // javascript→pythonへPNGデータ転送
+    var textData = JSON.stringify({'b64_pic':pic});
+//  console.log(textData);
+
+    $.ajax({
+      type:'POST',
+      url:'/output',
+      data:textData,
+      contentType:'application/json',
+
+      // python→javascriptへデータ返送
+      // 非同期通信が成功したら実行される
+      success:function(data){
+
+        // 返却jsonデータからparseしてデータ取り出し
+        var response = JSON.parse(data.ResultSet);
+//      console.log(response);
+
+        $('#ResultImg').css( { 'width' : '300' });
+        $('#exec').css( { 'cursor' : 'no-drop' });
+        document.getElementById('ResultImg').src = response.result_pic;
+        document.getElementById('link').href = response.result_pic;
+        document.getElementById('link').download = 'download.png';
+        document.getElementById('link').innerHTML = "Click to Download!!";
+//      console.log(response.result_pic);
+
+        var okng = response.result_okng;
+//      console.log(okng);
+      }
+    });
+  }
+
+  //---------------------------------------------------------------------------
+  // ファイルが選択されたときの処理
+  //---------------------------------------------------------------------------
+  const file_select = document.getElementById('file_select');
+
+  // ファイル選択 or キャンセルで実行される
+  const handleFileSelect = () => {
+    const files = file_select.files;
+    const freader = new FileReader();
+
+    // ファイルが読み込まれたときに実行する
+    freader.onload = function (e) {
+      const imageUrl = e.target.result;
+      document.getElementById('previewtImg').src = imageUrl;
+      document.getElementById('ResultImg').src = '';
+      document.getElementById('link').href = '';
+      document.getElementById('link').download = '';
+      document.getElementById('link').innerHTML = '';
+      document.getElementById('exec').disabled = false;
+      $('#exec').css( { 'cursor' : 'pointer' });
+//    console.log(imageUrl);
+    }
+
+    // ファイルが読み込み
+    if (files.length > 0) {
+      freader.readAsDataURL(files[0]);
+    }
+  }
+
+  // イベント登録
+  file_select.addEventListener('change', handleFileSelect);
+  $('#exec').css( { 'cursor' : 'no-drop' });
