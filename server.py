43d6a043ada30b6d30b558e4aef6eafba3d5c70a
Poodle Master <69660581+PoodleMaster@users.noreply.github.com>
2021-08-13 09:41:22 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 ############################################################
-# PoodleMaster
+# by PoodleMaster
 ############################################################
 from flask import Flask, request, render_template, jsonify
 from rembg.bg import remove
