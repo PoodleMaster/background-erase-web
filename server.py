62fa6faae736758da3944a872579d6bd6bf77070
Poodle Master <69660581+PoodleMaster@users.noreply.github.com>
2021-08-24 11:49:18 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 ############################################################
-# Copyright 2021 PoodleMaster. All rights reserved. 
+# Copyright 2021 PoodleMaster. All rights reserved.
 ############################################################
 from flask import Flask, request, render_template, jsonify
 from rembg.bg import remove
 import base64
 import json
-
 
 ############################################################
 # Flask
@@ -28,10 +27,10 @@ def index():
 def output():
     # json形式でデータを受け取る
     b64_data = request.json['b64_pic']
-#   display(b64_data, "b64_data")
+    #   display(b64_data, "b64_data")
 
     # base64デコード
-    tmpdata = b64_data.split(',')           # base64のヘッダを削除
+    tmpdata = b64_data.split(',')  # base64のヘッダを削除
     bindata = base64.b64decode(tmpdata[1])  # 「data:image/png;base64,～」以降のデータのみをデコード
 
     # rembgでバックグランドを削除
@@ -40,22 +39,22 @@ def output():
                       alpha_matting_foreground_threshold=240,
                       alpha_matting_background_threshold=10,
                       alpha_matting_erode_structure_size=10,
-                      alpha_matting_base_size=1000 )
+                      alpha_matting_base_size=1000)
 
     # base64エンコード
     tmpdata = str(base64.b64encode(contents))
-#   display(tmpdata, "tmpdata")
+    #   display(tmpdata, "tmpdata")
     tmpdata = tmpdata[2:-1]  # 「ｂ’～’」の中身だけをエンコード
 
     # 返却データ設定
     data1 = "data:image/png;base64," + tmpdata
     data2 = "OK"
 
-    return_data = { "result_pic": data1,
-                    "result_result": data2,
-                  }
+    return_data = {"result_pic": data1,
+                   "result_result": data2,
+                   }
 
-#   display(return_data, "return_data")
+    #   display(return_data, "return_data")
     return jsonify(ResultSet=json.dumps(return_data))
 
 
