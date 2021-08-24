dc82659dbb01971b62b76b6b936f60891fe2d194
Poodle Master <69660581+PoodleMaster@users.noreply.github.com>
2021-08-24 11:51:00 +0900

    Add files via upload

--- static/Exec.js
+++ static/Exec.js
@@ -4,186 +4,194 @@
 ############################################################
 */
 
-  //---------------------------------------------------------------------------
-  // 変数
-  //---------------------------------------------------------------------------
-  // リサイズ用
-  const FIX_SIZE = 512;
-  var canvas = document.createElement('canvas');
-  var ctx = canvas.getContext('2d');
-  var image = new Image();
-  image.crossOrigin = "Anonymous";
+//---------------------------------------------------------------------------
+// 変数
+//---------------------------------------------------------------------------
+// リサイズ用
+const FIX_SIZE = 512;
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+var image = new Image();
+image.crossOrigin = "Anonymous";
 
-  // 画像送信用
-  var send_pic;
+// 画像送信用
+var send_pic;
 
-  // 画像受信用
-  var response;
+// 画像受信用
+var response;
 
-  // モード用
-  var mode = 0;
-  var resize_flag = true;
+// モード用
+var mode = 0;
+var resize_flag = true;
 
+//---------------------------------------------------------------------------
+// タイトル 押下イベント
+//---------------------------------------------------------------------------
+function author() {
+  window.open("https://qiita.com/PoodleMaster");
+}
 
-  //---------------------------------------------------------------------------
-  // タイトル 押下イベント
-  //---------------------------------------------------------------------------
-  function author() {
-    window.open('https://qiita.com/PoodleMaster');
-  }
+//---------------------------------------------------------------------------
+// モード変更 押下イベント
+//---------------------------------------------------------------------------
+function mode_change() {
+  mode++;
 
-
-  //---------------------------------------------------------------------------
-  // モード変更 押下イベント
-  //---------------------------------------------------------------------------
-  function mode_change() {
-    mode++;
-
-    resize_flag = true;
-    if (mode < 10) {
-      document.getElementById('Good').innerHTML = '👍+' + String(mode);
+  resize_flag = true;
+  if (mode < 10) {
+    document.getElementById("Good").innerHTML = "👍+" + String(mode);
+  } else {
+    if (mode % 2 == 0) {
+      document.getElementById("Good").innerHTML = "😋";
     } else {
-      if ((mode % 2) == 0) {
-        document.getElementById('Good').innerHTML = '😋';
-      } else {
-        document.getElementById('Good').innerHTML = '😑';
-        resize_flag = false;
-      }
-    }
-    image.onload();
-  }
-
-
-  //---------------------------------------------------------------------------
-  // image.onloadイベント (送信画像設定)
-  //---------------------------------------------------------------------------
-  image.onload = function(){
-    var pic        = document.getElementById('previewtImg').src;
-    var pic_width  = document.getElementById('previewtImg').naturalWidth;
-    var pic_height = document.getElementById('previewtImg').naturalHeight;
-//  console.log(pic);
-    send_pic = pic;
-
-    var dst_width;
-    var dst_height;
-
-    if (resize_flag && ((pic_width > FIX_SIZE) || (pic_height > FIX_SIZE))) {
-      if (pic_width < pic_height) {
-        dst_width  = pic_width * (FIX_SIZE / pic_height);
-        dst_height = FIX_SIZE;
-      } else {
-        dst_width  = FIX_SIZE;
-        dst_height = pic_height * (FIX_SIZE / pic_width);
-      }
-      canvas.width  = dst_width;
-      canvas.height = dst_height;
-      ctx.drawImage(image, 0, 0, pic_width, pic_height, 0, 0, dst_width, dst_height);
-      send_pic = canvas.toDataURL();
-    }
-//  console.log(send_pic);
-
-    document.getElementById('exec').disabled = false;
-    $('#exec').css( { 'cursor' : 'pointer' });
-  }
-
-
-  //---------------------------------------------------------------------------
-  // Execボタン 押下イベント (切り抜き処理開始)
-  //---------------------------------------------------------------------------
-  function exec_button() {
-
-    document.getElementById('exec').disabled = true;
-    $('#exec').css( { 'cursor' : 'wait' });
-//  wait_pic = 'static/wait.gif?' + (new Date()).getTime();
-    wait_pic = 'static/wait.gif';
-    document.getElementById("ResultImg").className = "ResultImgWait";
-    document.getElementById('ResultImg').src = wait_pic;
-    document.getElementById('link').href       = '';
-    document.getElementById('link').download   = '';
-    document.getElementById('link').innerHTML  = '';
-  }
-
-  //---------------------------------------------------------------------------
-  // ResultImgLoadWait loadイベント
-  //---------------------------------------------------------------------------
-  function ResultImgLoadWait() {
-
-    document.getElementById('ResultImg').removeEventListener('load', ResultImgLoadWait);
-    document.getElementById('ResultImg').addEventListener('load', ResultImgLoadResult);
-
-    // JQueryによるPOST処理
-    // javascript→pythonへ画像データ転送
-    var textData = JSON.stringify({'b64_pic':send_pic});
-//  console.log(textData);
-
-    $.ajax({
-      type:'POST',
-      url:'/output',
-      data:textData,
-      contentType:'application/json',
-
-      // python→javascriptへデータ返送
-      // 非同期通信が成功したら実行される
-      success:function(data){
-
-        // 返却jsonデータからparseしてデータ取り出し
-        response = JSON.parse(data.ResultSet);
-//      console.log(response);
-//      console.log(response.result_pic);
-//      console.log(response.result_result);
-
-        $('#exec').css( { 'cursor' : 'no-drop' });
-        document.getElementById("ResultImg").className = "ResultImg";
-        document.getElementById('ResultImg').src  = response.result_pic;
-      }
-    });
-  }
-
-
-  //---------------------------------------------------------------------------
-  // ResultImgLoadResult loadイベント
-  //---------------------------------------------------------------------------
-  function ResultImgLoadResult() {
-
-    document.getElementById('ResultImg').removeEventListener('load', ResultImgLoadResult);
-    document.getElementById('ResultImg').addEventListener('load', ResultImgLoadWait);
-
-    document.getElementById('link').href      = response.result_pic;
-    document.getElementById('link').download  = 'download.png';
-    document.getElementById('link').innerHTML = "Click to Download!!";
-  }
-
-
-  //---------------------------------------------------------------------------
-  // ファイル選択処理
-  //---------------------------------------------------------------------------
-  const file_select = document.getElementById('file_select');
-
-  // ファイル選択 or キャンセルで実行される
-  const handleFileSelect = () => {
-    const files = file_select.files;
-    const freader = new FileReader();
-
-    // ファイルが読み込まれたときに実行する
-    freader.onload = function (e) {
-      document.getElementById('exec').disabled = true;
-      const imageUrl = e.target.result;
-      document.getElementById('previewtImg').src = imageUrl;
-      document.getElementById('ResultImg').src   = '';
-      document.getElementById('link').href       = '';
-      document.getElementById('link').download   = '';
-      document.getElementById('link').innerHTML  = '';
-      image.src = imageUrl;
-//    console.log(imageUrl);
-    }
-
-    // ファイルが読み込み
-    if (files.length > 0) {
-      freader.readAsDataURL(files[0]);
+      document.getElementById("Good").innerHTML = "😑";
+      resize_flag = false;
     }
   }
+  image.onload();
+}
 
-  // イベント登録
-  file_select.addEventListener('change', handleFileSelect);
-  document.getElementById('ResultImg').addEventListener('load', ResultImgLoadWait);
-  $('#exec').css( { 'cursor' : 'no-drop' });
+//---------------------------------------------------------------------------
+// image.onloadイベント (送信画像設定)
+//---------------------------------------------------------------------------
+image.onload = function () {
+  var pic = document.getElementById("previewtImg").src;
+  var pic_width = document.getElementById("previewtImg").naturalWidth;
+  var pic_height = document.getElementById("previewtImg").naturalHeight;
+  //  console.log(pic);
+  send_pic = pic;
+
+  var dst_width;
+  var dst_height;
+
+  if (resize_flag && (pic_width > FIX_SIZE || pic_height > FIX_SIZE)) {
+    if (pic_width < pic_height) {
+      dst_width = pic_width * (FIX_SIZE / pic_height);
+      dst_height = FIX_SIZE;
+    } else {
+      dst_width = FIX_SIZE;
+      dst_height = pic_height * (FIX_SIZE / pic_width);
+    }
+    canvas.width = dst_width;
+    canvas.height = dst_height;
+    ctx.drawImage(
+      image,
+      0,
+      0,
+      pic_width,
+      pic_height,
+      0,
+      0,
+      dst_width,
+      dst_height
+    );
+    send_pic = canvas.toDataURL();
+  }
+  //  console.log(send_pic);
+
+  document.getElementById("exec").disabled = false;
+  $("#exec").css({ cursor: "pointer" });
+};
+
+//---------------------------------------------------------------------------
+// Execボタン 押下イベント (切り抜き処理開始)
+//---------------------------------------------------------------------------
+function exec_button() {
+  document.getElementById("exec").disabled = true;
+  $("#exec").css({ cursor: "wait" });
+  //  wait_pic = 'static/wait.gif?' + (new Date()).getTime();
+  wait_pic = "static/wait.gif";
+  document.getElementById("ResultImg").className = "ResultImgWait";
+  document.getElementById("ResultImg").src = wait_pic;
+  document.getElementById("link").href = "";
+  document.getElementById("link").download = "";
+  document.getElementById("link").innerHTML = "";
+}
+
+//---------------------------------------------------------------------------
+// ResultImgLoadWait loadイベント
+//---------------------------------------------------------------------------
+function ResultImgLoadWait() {
+  document
+    .getElementById("ResultImg")
+    .removeEventListener("load", ResultImgLoadWait);
+  document
+    .getElementById("ResultImg")
+    .addEventListener("load", ResultImgLoadResult);
+
+  // JQueryによるPOST処理
+  // javascript→pythonへ画像データ転送
+  var textData = JSON.stringify({ b64_pic: send_pic });
+  //  console.log(textData);
+
+  $.ajax({
+    type: "POST",
+    url: "/output",
+    data: textData,
+    contentType: "application/json",
+
+    // python→javascriptへデータ返送
+    // 非同期通信が成功したら実行される
+    success: function (data) {
+      // 返却jsonデータからparseしてデータ取り出し
+      response = JSON.parse(data.ResultSet);
+      //      console.log(response);
+      //      console.log(response.result_pic);
+      //      console.log(response.result_result);
+
+      $("#exec").css({ cursor: "no-drop" });
+      document.getElementById("ResultImg").className = "ResultImg";
+      document.getElementById("ResultImg").src = response.result_pic;
+    }
+  });
+}
+
+//---------------------------------------------------------------------------
+// ResultImgLoadResult loadイベント
+//---------------------------------------------------------------------------
+function ResultImgLoadResult() {
+  document
+    .getElementById("ResultImg")
+    .removeEventListener("load", ResultImgLoadResult);
+  document
+    .getElementById("ResultImg")
+    .addEventListener("load", ResultImgLoadWait);
+
+  document.getElementById("link").href = response.result_pic;
+  document.getElementById("link").download = "download.png";
+  document.getElementById("link").innerHTML = "Click to Download!!";
+}
+
+//---------------------------------------------------------------------------
+// ファイル選択処理
+//---------------------------------------------------------------------------
+const file_select = document.getElementById("file_select");
+
+// ファイル選択 or キャンセルで実行される
+const handleFileSelect = () => {
+  const files = file_select.files;
+  const freader = new FileReader();
+
+  // ファイルが読み込まれたときに実行する
+  freader.onload = function (e) {
+    document.getElementById("exec").disabled = true;
+    const imageUrl = e.target.result;
+    document.getElementById("previewtImg").src = imageUrl;
+    document.getElementById("ResultImg").src = "";
+    document.getElementById("link").href = "";
+    document.getElementById("link").download = "";
+    document.getElementById("link").innerHTML = "";
+    image.src = imageUrl;
+    // console.log(imageUrl);
+  };
+
+  // ファイルが読み込み
+  if (files.length > 0) {
+    freader.readAsDataURL(files[0]);
+  }
+};
+
+// イベント登録
+file_select.addEventListener("change", handleFileSelect);
+document.getElementById("ResultImg").addEventListener("load", ResultImgLoadWait);
+$("#exec").css({ cursor: "no-drop" });
